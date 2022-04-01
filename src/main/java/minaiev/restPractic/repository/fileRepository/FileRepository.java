c551d277af379db4ece8513cb56691dbd1ba1c9a
Oleksandr Minaiev <minaev@gmail.com>
2022-04-01 15:31:40 +0200

    addFileRepo

---
+++ src/main/java/minaiev/restPractic/repository/fileRepository/FileRepository.java
@@ -0,0 +1,21 @@
+package minaiev.restPractic.repository.fileRepository;
+
+import java.io.File;
+
+public class FileRepository {
+
+public File downloadFile(File file){
+return file;
+}
+
+public File uploadFile(){
+    return null;
+}
+
+public File renameFile (File file){
+    return file;
+}
+
+public void deleteFile(File file){}
+
+}
