5ab88e0027bb8b308ca86c33ef715f3313eea1cd
Oleksandr Minaiev <minaev@gmail.com>
2022-04-01 14:24:41 +0200

    addSQLRepo

--- src/main/java/minaiev/restPractic/repository/SQLRepository/SQLRepositoryImpl/EventRepositoryImpl.java
+++ src/main/java/minaiev/restPractic/repository/SQLRepository/SQLRepositoryImpl/EventRepositoryImpl.java
@@ -1,6 +1,10 @@
 package minaiev.restPractic.repository.SQLRepository.SQLRepositoryImpl;
 
 import minaiev.restPractic.model.Event;
+import minaiev.restPractic.util.SQLUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,13 +12,23 @@ import java.util.List;
 public class EventRepositoryImpl  {
 
     public List<Event> getEventByUserId(Integer userId) {
-        List<Event> eventsByUserId  = new ArrayList<>();
+        Session session = SQLUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        String qv = String.format("FROM events E WHERE E.user_id = %d", userId);
+        Query query = session.createQuery(qv);
+        List eventsByUserId = query.list();
+        session.close();
         return eventsByUserId;
     }
 
 
     public List<Event> getEventByFileId(Integer fileId) {
-        List<Event> eventsByFileId = new ArrayList<>();
+        Session session = SQLUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        String qv = String.format("FROM events E WHERE E.file_id = %d", fileId);
+        Query query = session.createQuery(qv);
+        List eventsByFileId = query.list();
+        session.close();
         return eventsByFileId;
     }
 }
--- src/main/java/minaiev/restPractic/repository/SQLRepository/SQLRepositoryImpl/FileRepositoryImpl.java
+++ src/main/java/minaiev/restPractic/repository/SQLRepository/SQLRepositoryImpl/FileRepositoryImpl.java
@@ -2,32 +2,58 @@ package minaiev.restPractic.repository.SQLRepository.SQLRepositoryImpl;
 
 import minaiev.restPractic.model.File;
 import minaiev.restPractic.repository.SQLRepository.FileRepository;
+import minaiev.restPractic.util.SQLUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class FileRepositoryImpl implements FileRepository {
     @Override
-    public File getById(Integer integer) {
-        return null;
+    public File getById(Integer id) {
+        Session session = SQLUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        File file = session.get(File.class, id);
+        transaction.commit();
+        session.close();
+        return file;
     }
 
     @Override
     public File update(File file) {
+        Session session = SQLUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(file);
+        transaction.commit();
+        session.close();
         return null;
     }
 
     @Override
-    public void deleteById(Integer integer) {
-
+    public void deleteById(Integer id) {
+        Session session = SQLUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(session.get(File.class, id));
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public List<File> getAll() {
+        Session session = SQLUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        List files = session.createQuery("FROM files").list();
+        session.close();
         return null;
     }
 
     @Override
     public File save(File file) {
+        Session session = SQLUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        file.setId((Integer)session.save(file));
+        transaction.commit();
+        session.close();
         return null;
     }
 }
